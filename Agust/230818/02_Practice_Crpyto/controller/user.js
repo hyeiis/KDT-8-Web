5e60ae98cfad96ff802ce9764ca00938c6c0a6f5
hyeiis <hyeis0807@gmail.com>
2023-08-21 08:59:09 +0900

    Feat:230819_practice crypto

--- Agust/230818/02_Practice_Crpyto/controller/user.js
+++ Agust/230818/02_Practice_Crpyto/controller/user.js
@@ -1,5 +1,64 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
 exports.index = (req, res) => {
   res.render("index");
+};
+
+exports.get_signup = (req, res) => {
+  res.render("signup");
+};
+
+exports.get_signin = (req, res) => {
+  res.render("signin");
+};
+
+// 회원가입
+exports.register = async (req, res) => {
+  try {
+    const { userid, password, name } = req.body;
+    const hashPw = bcryptPassword(password);
+    const result = await User.create({
+      userid,
+      name,
+      password: hashPw,
+    });
+    if (result) {
+      res.json({ result: true });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// 로그인
+exports.login = async (req, res) => {
+  try {
+    const { userid, password } = req.body;
+    // 사용자 조회
+    const result = await User.findOne({
+      where: { userid },
+    });
+    console.log("user: ", result);
+    if (!result) {
+      res.json({ result: false, message: "사용자가 존재하지 않습니다" });
+    }
+    // 비밀번호 확인
+    const compare = comparePassword(password, result.password);
+    if (compare) {
+      res.json({ result: true });
+    } else {
+      res.json({ result: false, message: "비밀번호가 일치하지 않습니다" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const bcryptPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+const comparePassword = (password, dbPw) => {
+  return bcrypt.compareSync(password, dbPw);
 };
