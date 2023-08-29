4f31bee234f2ca47724c007859f486042c1ed2e0
hyeiis <hyeis0807@gmail.com>
2023-08-29 09:15:55 +0900

    Edit app.js

--- Agust/230828/01_WebSocket/app.js
+++ Agust/230828/01_WebSocket/app.js
@@ -1,26 +1,17 @@
+const http = require("http");
 const express = require("express");
 const ws = require("ws");
 const app = express();
+const server = http.createServer(app);
 const PORT = 8000;
+// websocket 서버 연결
+const wss = new ws.Server({ server });
+// 브라우저(클라이언트)들을 담을 변수 선언
+const sockets = [];
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.render("client");
-});
-
-// app.listen 밑에 event 관련 코드 작성 가능. 미들웨어 x
-const server = app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
-
-// websocket 서버 연결
-const wss = new ws.Server({ server });
-
-// 브라우저(클라이언트)들을 담을 변수 선언
-const sockets = [];
 
 // 서버에서 이벤트 불러올 때는 on 이용
 // socket 변수는 접속한 브라우저
@@ -44,4 +35,13 @@ wss.on("connection", (socket) => {
   socket.on("close", () => {
     console.log("클라이언트와의 연결이 종료되었습니다");
   });
+});
+
+app.get("/", (req, res) => {
+  res.render("client");
+});
+
+// app.listen 밑에 event 관련 코드 작성 가능. 미들웨어 x
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
