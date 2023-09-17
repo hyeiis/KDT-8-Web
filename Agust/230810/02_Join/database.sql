781fa06947b65d3fd4ef3e57494b9debf649b145
hyeiis <hyeis0807@gmail.com>
2023-09-18 08:50:15 +0900

    Edit folder name

--- Agust/230810/02_Join/database.sql
+++
@@ -1,6 +0,0 @@
-USE db1;
-
-SELECT a.name as "학생명", a.major as "전공", a.enroll as "입학년도", b.name as "수업명", b.room as "강의실", b.code, b.teacher_name 
-FROM students as a
-INNER JOIN classes as b
-ON a.id = b.student_id;
--- Agust/Backend Project/routes/student.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controller/Cstudent");
-
-router.get("/", controller.index);
-router.post("/student", controller.post_student);
-router.post("/class", controller.post_class);
-
-module.exports = router;
