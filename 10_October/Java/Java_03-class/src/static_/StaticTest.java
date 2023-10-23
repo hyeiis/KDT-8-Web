255e8e11405b65ec1dd7301574e2cd729904a340
hyeiis <hyeis0807@gmail.com>
2023-10-24 08:50:46 +0900

    Java: static

--- 10_October/Java/Java_03-class/src/static_/StaticTest.java
+++ 10_October/Java/Java_03-class/src/static_/StaticTest.java
@@ -1,2 +1,26 @@
-package static_;public class StaticTest {
+package static_;
+
+class Student {
+  private String name;
+  // static 변수는 클래스의 모든 instance에서 공통으로 사용
+  private static int studentCount = 0;
+
+  public Student(String name) {
+    this.name = name;
+    this.studentCount ++;
+  }
+
+  public static int getStudentCount() {
+    return studentCount;
+  }
+}
+
+public class StaticTest {
+  public static void main(String[] args) {
+    Student s1 = new Student("홍길금");
+    Student s2 = new Student("홍길은");
+    Student s3 = new Student("홍길동");
+
+    System.out.println(Student.getStudentCount() + "명");
+  }
 }
