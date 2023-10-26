a45921f6202b37c20333b24ec36df37fd1c50bc7
hyeiis <hyeis0807@gmail.com>
2023-10-26 13:40:31 +0900

    Edit code

--- 10_October/Java/Java_03-class/src/collection/Collection_Practice2.java
+++ 10_October/Java/Java_03-class/src/collection/Collection_Practice2.java
@@ -1,13 +1,13 @@
 package collection;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Collection_Practice2 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    Map<String, Integer> user = new TreeMap<>();
+    Map<String, Integer> user = new HashMap<>();
 
     while(true) {
       System.out.println("사용자의 이름과 나이를 띄어쓰기로 구분하여 입력하세요");
